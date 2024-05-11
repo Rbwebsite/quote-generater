97b4b93785de8952d55fc0e2e8435fabee066b22
rohit <rohitsb96134@gmail.com>
2024-05-11 18:40:28 +0530

    update

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const link = "https://api.quotable.io/random"
 const btn = document.getElementById("btn")
 const Quote = document.getElementById("Quote")
 const author = document.getElementById("author")
+const copyButton = document.getElementById('copyQuote');
 
 
 function getQuote(){
@@ -20,4 +21,17 @@ function getQuote(){
 .catch((error)=>{console.log(error);})
 }
 
+copyButton.addEventListener('click', () => {
+    const textToCopy = Quote.innerHTML;
+    const textarea = document.createElement('textarea');
+    textarea.value = textToCopy;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    // document.body.removeChild(textarea);
+    alert('Quote copied to clipboard!');
+});
+
+
 btn.addEventListener('click',getQuote)
+
